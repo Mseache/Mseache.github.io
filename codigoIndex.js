8595835938820133fa09073a66f99210253bcb37
Manuel Christensen <manuchristensen1@gmail.com>
2023-10-13 18:47:01 -0300

    Js y primeros vinculos
    Agrega codigo basico de js para ejecutar los primeros vinculos y agrega un archivo html para el area de ka pagina de codigo

---
+++ codigoIndex.js
@@ -0,0 +1,24 @@
+var articulo_horario = document.getElementById("ArticuloHorario");
+var articulo_xhendra = document.getElementById("ArticuloXhendra");
+var articulo_campus_ap = document.getElementById("ArticuloCampusAP");
+var articulo_classroom = document.getElementById("ArticuloClassroom");
+
+articulo_horario.addEventListener("click", function() {
+  window.location.href = "ARTICULOhorario.html";
+  
+});
+
+articulo_xhendra.addEventListener("click", function() {
+    window.location.href = "https://xhendra.ar/";
+    
+});
+
+articulo_campus_ap.addEventListener("click", function() {
+    window.location.href = "https://aprendeprogramando.bue.edu.ar/";
+    
+});
+
+articulo_classroom.addEventListener("click", function() {
+    window.location.href = "https://classroom.google.com/h";
+    
+});
