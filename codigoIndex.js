9a9b8ad41fa2812d7485fcf5b4fe67ee7cfd8169
Manuel Christensen <manuchristensen1@gmail.com>
2023-10-16 20:25:02 -0300

    Agrega error404 a botones de inicio

--- codigoIndex.js
+++ codigoIndex.js
@@ -3,6 +3,15 @@ var articulo_xhendra = document.getElementById("ArticuloXhendra");
 var articulo_campus_ap = document.getElementById("ArticuloCampusAP");
 var articulo_classroom = document.getElementById("ArticuloClassroom");
 
+var botones = document.querySelectorAll(".indice");
+
+botones.forEach( function (boton) {
+    boton.addEventListener("click", function () {
+        window.location.href ="./error404.html"
+
+    });
+});
+
 articulo_horario.addEventListener("click", function() {
   window.location.href = "ARTICULOhorario.html";
   
@@ -22,3 +31,4 @@ articulo_classroom.addEventListener("click", function() {
     window.location.href = "https://classroom.google.com/h";
     
 });
+
